115b844a1ffbb0069ee7543fda02fa2badc52c9b
JaredVahle <jaredjohnvahle@gmail.com>
2021-09-20 09:16:54 -0500

    updating work

---
+++ wrangle_mall.py
@@ -0,0 +1,57 @@
+import os
+import pandas as pd
+import env
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+def get_connection(db, user=env.user, host=env.host, password=env.password):
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+def get_mallcustomer_data():
+    df = pd.read_sql('SELECT * FROM customers;', get_connection('mall_customers'))
+    return df.set_index('customer_id')
+
+
+def train_validate_test(df, target):
+    # split df into test (20%) and train_validate (80%)
+    train_validate, test = train_test_split(df, test_size=.2, random_state=174)
+    # split train/validate into train (60%) and validate (20%)
+    train, validate = train_test_split(train_validate, test_size=.25, random_state=174)
+    # splits our target off of our train, validate, test
+    X_train = train.drop(columns=[target])
+    y_train = train[target]
+    
+    X_validate = validate.drop(columns=[target])
+    y_validate = validate[target]
+    
+    X_test = test.drop(columns=[target])
+    y_test = test[target]
+    
+    return train, validate, test, X_train, y_train, X_validate, y_validate, X_test, y_test
+
+def min_max_scaler(X_train, X_validate, X_test):
+    """
+    Takes in X_train, X_validate and X_test dfs with numeric values only
+    Returns scaler, X_train_scaled, X_validate_scaled, X_test_scaled dfs 
+    """
+    scaler = sklearn.preprocessing.MinMaxScaler().fit(X_train)
+    X_train_scaled = pd.DataFrame(scaler.transform(X_train), index = X_train.index, columns = X_train.columns)
+    X_validate_scaled = pd.DataFrame(scaler.transform(X_validate), index = X_validate.index, columns = X_validate.columns)
+    X_test_scaled = pd.DataFrame(scaler.transform(X_test), index = X_test.index, columns = X_test.columns)
+    
+    return scaler, X_train_scaled, X_validate_scaled, X_test_scaled
+
+def handle_outliers(df, col):
+    q1 = df[col].quantile(.25)
+    q3 = df[col].quantile(.75)
+    iqr = q3-q1 #Interquartile range
+    lower_bound  = q1-1.5*iqr
+    upper_bound = q3+1.5*iqr
+    if lower_bound < 0:
+        lower_bound = 0
+    if upper_bound > df[col].max():
+        upper_bound = df[col].max()
+    df_out = df.loc[(df[col] > lower_bound) & (df[col] < upper_bound)]
+    return df_out
